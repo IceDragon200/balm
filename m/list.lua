e277f4fe1e3cc66f5fbc61a2759616cc995b5d24
Corey Powell <mistdragon100@gmail.com>
2026-06-29 15:23:30 -0500

    Promote some math functions to local variables

--- m/list.lua
+++ m/list.lua
@@ -1,3 +1,8 @@
+local floor = assert(math.floor)
+local random = assert(math.random)
+local min = assert(math.min)
+local max = assert(math.max)
+
 --- @namespace balm.m.list
 local m = {}
 
@@ -49,7 +54,7 @@ function m.crawford_base32_le_rolling_encode_table(...)
     i = i + 2
     -- determine the number segments that is the integer's byte count (as bits)
     -- divided by the base32 bit length (5), floored
-    segments = math.floor(ilen * 8 / 5)
+    segments = floor(ilen * 8 / 5)
 
     if type(item) == "string" then
       str = item
@@ -68,7 +73,7 @@ function m.crawford_base32_le_rolling_encode_table(...)
       error("unexpected item")
     end
 
-    int = math.floor(int)
+    int = floor(int)
 
     for _ = 1,segments do
       -- add 5 bits of the int to the accumulator
@@ -76,9 +81,9 @@ function m.crawford_base32_le_rolling_encode_table(...)
       -- modulo the accumulator to get the next value
       value = acc % 32
       -- reduce the accumulator
-      acc = math.floor(acc / 32)
+      acc = floor(acc / 32)
       -- reduce the primary integer
-      int = math.floor(int / 32)
+      int = floor(int / 32)
       -- write the encoded value
       result[r] = CROCKFORD_BASE32_ENCODE_TABLE[value]
       -- increment the result index
@@ -88,7 +93,7 @@ function m.crawford_base32_le_rolling_encode_table(...)
 
   while acc > 0 do
     value = acc % 32
-    acc = math.floor(acc / 32)
+    acc = floor(acc / 32)
     result[r] = CROCKFORD_BASE32_ENCODE_TABLE[value]
     r = r + 1
   end
@@ -113,7 +118,7 @@ local list_slice = m.slice
 --- @spec list_last(Table, count: Integer): Any
 function m.last(t, count)
   if count then
-    count = math.min(#t, count)
+    count = min(#t, count)
     return list_slice(t, #t - count + 1, count)
   else
     return t[#t]
@@ -163,7 +168,7 @@ end
 --- @spec list_sample(list: Table): Any
 function m.sample(list)
   local c = #list
-  return list[math.random(c)]
+  return list[random(c)]
 end
 
 --- Retrieves the next value after the specified 'current'
@@ -233,7 +238,7 @@ function m.split(list, alen)
 
   if source_len > 0 then
     if alen > 0 then
-      for i = 1,math.min(alen, source_len) do
+      for i = 1,min(alen, source_len) do
         head[i] = list[i]
       end
     end
@@ -385,7 +390,7 @@ function m.bsearch_by(t, predicate)
     local elem
     local res
     while lo <= hi do
-      idx = lo + math.floor((hi - lo) / 2)
+      idx = lo + floor((hi - lo) / 2)
       elem = t[idx]
 
       res = predicate(elem, idx)
